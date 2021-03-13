2ec94f6c9ac2825072c75f5e8d061bd8daa01ba8
Mwash01 <77361568+Mwash01@users.noreply.github.com>
2021-03-13 03:08:31 -0600

    Add files via upload

---
+++ Assets/Scripts/TimeScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class TimeScript : MonoBehaviour
+{
+    public Text TimeText;
+
+    [SerializeField]
+    public static float timeRemaining;
+    
+    void Start()
+    {
+        TimeText.text = "Time Remaining: " + Data.timeLimit;
+        timeRemaining = Data.timeLimit;
+    }
+
+    void Update() {
+        timeRemaining = timeRemaining - Time.deltaTime;
+        TimeText.text = "Time Remaining: " + timeRemaining.ToString("F2");
+        if (timeRemaining <= 0) {
+            SceneManager.LoadScene("Credits");
+        }
+    }
+}
