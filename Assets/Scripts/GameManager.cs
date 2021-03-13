1c9b2904dd6b34cf462dc1622c0636e339356003
Mwash01 <77361568+Mwash01@users.noreply.github.com>
2021-03-13 01:46:50 -0600

    Add files via upload

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    private bool gameHasEnded = false;
+
+    public Rotator rotator;
+    public Spawner spawner;
+    public Animator animator;
+
+    public void EndGame() {
+        if(gameHasEnded) {
+            return;
+        }
+        rotator.enabled = false;
+        spawner.enabled = false;
+        Data.scoreNum--;
+
+        animator.SetTrigger("EndGame");
+
+        gameHasEnded = true;
+    }
+
+    public void RestartGame() {
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        Data.playerLives--;
+        if(Data.playerLives == 0) {
+            SceneManager.LoadScene("Credits");
+        }
+        else {
+            spawner.enabled = true;
+            rotator.enabled = true;
+            gameHasEnded = false;
+        }
+    }
+}
